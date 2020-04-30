af6bbe227eb6ae5f14575d0cd7dd1ffa0dc53830
Alexey R <alexey_r.98@ngs.ru>
2020-04-30 20:35:23 +0700

    Rewrite grafana container. Added plugins support

--- grafana/start.sh
+++ grafana/start.sh
@@ -1,2 +1,24 @@
 #!/bin/sh
-exec /usr/sbin/grafana-server
+GRAFANA_INSTALL_PLUGINS=(
+  vertamedia-clickhouse-datasource
+)
+
+if [ ! -z "${GRAFANA_INSTALL_PLUGINS}" ]; then
+  # OLDIFS=$IFS
+  # IFS=','
+  for plugin in ${GRAFANA_INSTALL_PLUGINS}; do
+    # IFS=$OLDIFS
+    grafana-cli --pluginsDir "${GRAFANA_PLUGINS}" --config="$GRAFANA_CONFIG" \
+    plugins install ${plugin}
+  done
+fi
+
+exec /usr/sbin/grafana-server                               \
+  --homepath="$GRAFANA_HOME"                                \
+  --config="$GRAFANA_CONFIG"                                \
+  "$@"                                                      \
+  cfg:default.log.mode="console"                            \
+  cfg:default.paths.data="$GRAFANA_DATA"                    \
+  cfg:default.paths.logs="$GRAFANA_LOGS"                    \
+  cfg:default.paths.plugins="$GRAFANA_PLUGINS"              \
+  cfg:default.paths.provisioning="$GRAFANA_PROVISIONING"

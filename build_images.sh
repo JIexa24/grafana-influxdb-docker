7cc12c0530aa5af4469286d92bd2f87fdca290c8
Alexey R <alexey_r.98@ngs.ru>
2020-05-13 19:23:10 +0700

    small fixes

--- build_images.sh
+++ build_images.sh
@@ -2,5 +2,8 @@
 PREFIX=r9odt
 . ./images.dat
 for ((i = 0; i < ${#IMAGES[@]}; i+=3)); do
-  docker build -t $PREFIX/${IMAGES[i]}:${IMAGES[i+2]} -t $PREFIX/${IMAGES[i]}:latest --build-arg IMAGE_VERSION=${IMAGES[i+1]} ./${IMAGES[i]}
+  test -d ${IMAGES[i]}
+  if [[ $? == 0 ]]; then
+    docker build -t $PREFIX/${IMAGES[i]}:${IMAGES[i+2]} -t $PREFIX/${IMAGES[i]}:latest --build-arg IMAGE_VERSION=${IMAGES[i+1]} ./${IMAGES[i]}
+  fi
 done
--- run_compose.sh
+++ run_compose.sh
@@ -4,4 +4,4 @@ for ((i = 0; i < ${#IMAGES[@]}; i+=3)); do
   export ${IMAGES[i]}_version=${IMAGES[i+2]}
 done
 
-docker-compose up -d $1
+docker-compose up -d $*

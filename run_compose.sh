4ef8c0219fe3ef4c1309ff33f0c05d87d40850a8
Alexey R <alexey_r.98@ngs.ru>
2020-07-08 09:58:10 +0700

    images config

--- run_compose.sh
+++ run_compose.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 . ./images.dat
-for ((i = 0; i < ${#IMAGES[@]}; i+=3)); do
-  export ${IMAGES[i]}_version=${IMAGES[i+2]}
+for ((i = 0; i < ${#IMAGES[@]}; i+=4)); do
+  export ${IMAGES[i]}_image=${IMAGES[i+3]}
 done
 
 docker-compose up -d $*
